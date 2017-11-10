8d680a1681e952ce4cd72f46f026a6632ac54155
Kaitlyn Folsom <kaitlynfolsom@Kaitlyns-MacBook-Pro.local>
2017-11-09 20:26:34 -0500

    please

--- db/index.js
+++ db/index.js
@@ -8,7 +8,7 @@ const MONGO_LOCAL_URL = 'mongodb://localhost/pills'
 
 if (process.env.MONGODB_URI) {
 	mongoose.connect(process.env.MONGODB_URI)
-	MONGO_URL = process.env.MONGODB_URI
+	MONGOBD_URI = process.env.MONGODB_URI
 } else {
 	mongoose.connect(MONGO_LOCAL_URL) // local mongo url
 	MONGO_URL = MONGO_LOCAL_URL
